37ddb561c63b6db86e99dc9ac7aa28ed90c86a2c
David John Gagne <djgagne@ou.edu>
2019-08-16 14:34:53 -0600

    Tested training generator to find potential source of bad loss.

--- marbleri/test_training.py
+++ marbleri/test_training.py
@@ -1,6 +1,11 @@
-from .training import partition_storm_examples
+from .training import partition_storm_examples, BestTrackSequence
+from .process import get_hwrf_filenames
 import pandas as pd
+import xarray as xr
 import numpy as np
+from os.path import exists, join
+from os import makedirs
+from .models import BaseConvNet
 
 
 def test_partition_storm_examples():
@@ -10,4 +15,50 @@ def test_partition_storm_examples():
     assert train_indices[0].size > 0
     assert val_indices[0].size > 0
     assert len(shared) == 0
+    return
+
+
+def test_best_track_sequence():
+    best_track_data = pd.read_csv("testdata/best_track_test.csv", index_col="Index")
+    input_cols = ["THETA_E_L100_50000", "THETA_E_L100_70000", "THETA_E_L100_85000",
+                 "U_RAD_L100_50000", "U_RAD_L100_70000", "U_RAD_L100_85000",
+                 "V_TAN_L100_50000", "V_TAN_L100_70000", "V_TAN_L100_85000"]
+    output_col = "vmax_bt_new"
+    data_width = 16
+    path = "testdata/hwrf_norm"
+    batch_size = 2
+    def create_synthetic_netcdfs(best_track_data, input_cols, data_width, path):
+        hwrf_filenames = get_hwrf_filenames(best_track_data, hwrf_path=path)
+        for hwrf_filename in hwrf_filenames:
+            out_data = np.random.normal(size=(len(input_cols), data_width, data_width))
+            out_data[3, 5, 5] = np.nan
+            out_data[4, 6, 10] = np.inf
+            ds = xr.DataArray(out_data, dims=("variable", "y", "x"), coords={"variable": input_cols,
+                                                                                    "y": np.arange(data_width),
+                                                                                    "x": np.arange(data_width)},
+                              name="hwrf_norm")
+            ds.to_netcdf(hwrf_filename,
+                         encoding={"hwrf_norm": {"zlib": True, "complevel": 3}}
+                         )
+        return
+
+    if not exists(path):
+        makedirs(path)
+    create_synthetic_netcdfs(best_track_data, input_cols, data_width, path)
+    bts = BestTrackSequence(best_track_data, None, [], output_col, input_cols, batch_size, path,
+                            domain_width=data_width)
+    assert len(bts) == best_track_data.shape[0] // batch_size
+    # assert np.count_nonzero(np.isnan(bts.conv_inputs)) == 0
+    for i in range(len(bts)):
+        batch = bts[i]
+        assert batch[0].shape == (batch_size, len(input_cols), data_width, data_width)
+        assert np.count_nonzero(~np.isfinite(batch[0])) == 0
+        assert np.count_nonzero(~np.isfinite(batch[1])) == 0
+    bcn = BaseConvNet(min_filters=4, verbose=1, epochs=5)
+    bcn.fit_generator(bts)
+    for layer in bcn.model.layers:
+        weights = layer.get_weights()
+        if len(weights) > 0:
+            for weight in weights:
+                assert np.count_nonzero(~np.isfinite(weight)) == 0
     return
--- marbleri/training.py
+++ marbleri/training.py
@@ -1,6 +1,5 @@
 import numpy as np
 from keras.utils import Sequence
-from .nwp import HWRFStep
 from .process import get_hwrf_filenames
 import xarray as xr
 from os.path import exists
@@ -44,6 +43,10 @@ def partition_storm_examples(best_track_data, num_ranks, validation_proportion=0
 
 
 class BestTrackSequence(Sequence):
+    """
+    Generator for HWRF convolutional neural network training.
+
+    """
     def __init__(self, best_track_data, best_track_scaler, best_track_inputs, best_track_output,
                  hwrf_inputs, batch_size, hwrf_path,
                  conv_only=True, shuffle=True, data_format="channels_first", domain_width=384):
@@ -88,10 +91,10 @@ class BestTrackSequence(Sequence):
                 hwrf_ds.close()
             else:
                 logging.info(hwrf_file_name + " does not exist")
-        nan_points = np.count_nonzero(np.isnan(self.conv_inputs))
+        nan_points = np.count_nonzero(~np.isfinite(self.conv_inputs))
         if nan_points > 0:
             print(f"Number of nan points: {nan_points:d}") 
-            self.conv_inputs[np.isnan(self.conv_inputs)] = 0
+            self.conv_inputs[~np.isfinite(self.conv_inputs)] = 0
         if self.shuffle:
             np.random.shuffle(self.indices)
 
@@ -112,67 +115,4 @@ class BestTrackSequence(Sequence):
     def on_epoch_end(self):
         if self.shuffle:
             np.random.shuffle(self.indices)
-
-
-class HWRFSequence(Sequence):
-    """
-
-
-    """
-    def __init__(self, hwrf_files, best_track, hwrf_variables, hwrf_variable_levels,
-                 best_track_input_variables, best_track_label_variables, batch_size,
-                 x_start=0, x_end=601, shuffle=True):
-        self.hwrf_files = hwrf_files
-        self.best_track = best_track
-        self.hwrf_variables = hwrf_variables
-        self.hwrf_variable_levels = hwrf_variable_levels
-        self.num_hwrf_variables = len(self.hwrf_variables)
-        self.best_track_input_variables = best_track_input_variables
-        self.num_best_track_input_variables = len(best_track_input_variables)
-        self.best_track_label_variables = best_track_label_variables
-        self.num_best_track_label_variables = len(best_track_label_variables)
-        self.batch_size = batch_size
-        self.x_subset = (x_start, x_end)
-        self.x_size = x_end - x_start
-        self.shuffle = True
-        self.indexes = np.arange(len(self.hwrf_files))
-        self.on_epoch_end()
-        self.shuffle = shuffle
-
-    def __len__(self):
-        return int(np.floor(len(self.hwrf_files) / self.batch_size))
-
-    def __getitem__(self, index):
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Generate data
-        X, y = self.__data_generation(indexes)
-        return X, y
-
-    def on_epoch_end(self):
-        self.indexes = np.arange(len(self.hwrf_files))
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, index_list_temp):
-        all_hwrf_patches = np.zeros((index_list_temp.size, self.x_size, self.x_size, self.num_hwrf_variables), dtype=np.float32)
-        all_best_track_input_data = np.zeros((index_list_temp.size, self.num_best_track_input_variables), dtype=np.float32)
-        all_best_track_label_data = np.zeros((index_list_temp.size, self.num_best_track_label_variables), dtype=np.float32)
-        for i, idx in enumerate(index_list_temp):
-            hwrf_file = self.hwrf_files[idx]
-            hwrf_data = HWRFStep(hwrf_file)
-            for v, variable in enumerate(self.hwrf_variables):
-                all_hwrf_patches[i, :, :, v] = hwrf_data.get_variable(variable, level=self.hwrf_variable_levels[v],
-                                                            subset=self.x_subset)
-                all_hwrf_patches[i, :, :, v][np.isnan(all_hwrf_patches[i, :, :, v])] = np.nanmin(all_hwrf_patches[i, :, :, v])
-            hwrf_data.close()
-            all_best_track_input_data[i] = self.best_track.get_storm_variables(self.best_track_input_variables,
-                                            hwrf_data.run_date, hwrf_data.storm_name, hwrf_data.storm_number,
-                                            hwrf_data.basin, hwrf_data.forecast_hour)
-            all_best_track_label_data[i] = self.best_track.get_storm_variables(self.best_track_label_variables,
-                                                                    hwrf_data.run_date, hwrf_data.storm_name,
-                                                                    hwrf_data.storm_number,
-                                                                    hwrf_data.basin, hwrf_data.forecast_hour)
-        return [all_hwrf_patches, all_best_track_input_data], all_best_track_label_data
-
 
